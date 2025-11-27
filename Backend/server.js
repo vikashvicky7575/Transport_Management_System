755fc1531949dd8195d1fd7f1282270f00373216
Vikash kumar M <vikash kumar@smt.org>
2025-11-27 16:42:19 +0530

    Update Server File

--- Backend/server.js
+++ Backend/server.js
@@ -41,5 +41,5 @@ app.use("/api/tripSchedule", tripRoutes);
 app.use("/api/mainteances", maintenanceRoutes);
 
 // Start Server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
