21e25dcca60847190218e38e0326d7f7c695e680
Vikash kumar M <vikash kumar@smt.org>
2025-12-05 15:15:45 +0530

    Image Uploaded

--- frontend/src/Admin_Panel/Pages/DispatchPanel/Mainteances/MainteanceHistory/MainteanceHistory.jsx
+++ frontend/src/Admin_Panel/Pages/DispatchPanel/Mainteances/MainteanceHistory/MainteanceHistory.jsx
@@ -1,85 +1,3 @@
-// import React, { useEffect } from "react";
-// // import styles from "./MainteanceHistory.module.css";
-// import { fetchMaiteancesHistoryRequest } from "../../../../../Redux/Slice/mainteancesSlice";
-// import { useSelector, useDispatch } from "react-redux";
-
-// const MainteanceHistory = () => {
-//   const dispatch = useDispatch();
-//   const { loading, error, mainteanceAllHistory } = useSelector(
-//     (state) => state.mainteancesStore
-//   );
-
-//   console.log("MainteancesDetails:", mainteanceAllHistory);
-
-//   useEffect(() => {
-//     dispatch(fetchMaiteancesHistoryRequest());
-//   }, [dispatch]);
-
-//   return (
-//     <>
-//       <div className="container mt-4">
-//         <h3>All Mainteances History</h3>
-//         {loading && <p>Loading...</p>}
-//         {error && <p className="text-danger">Error: {error}</p>}
-
-//         {mainteanceAllHistory.length === 0 && !loading ? (
-//           <p>No Mainteances History</p>
-//         ) : (
-//           <table className="table table-borderd mt-3">
-//             <thead>
-//               <tr className="text-align-center">
-//                 <th>Mainteanice ID</th>
-//                 <th>Brand Name</th>
-//                 <th>Vehicle Type</th>
-//                 <th>Vehicle Number</th>
-//                 <th>Vehicle Image</th>
-//                 <th>Type</th>
-//                 <th>Date</th>
-//                 <th>Issue</th>
-//                 <th>Company</th>
-//                 <th>Mechanic</th>
-//                 <th>Status</th>
-//               </tr>
-//             </thead>
-//             <tbody>
-//               {mainteanceAllHistory.map((m) => (
-//                 <tr key={m._id}>
-//                   <td>{m.mainteananceId}</td>
-//                   <td>{m.vehicleId?.brandName}</td>
-//                   <td>{m.vehicleId?.vehicleType}</td>
-//                   <td>{m.vehicleId?.vehicleNumber}</td>
-//                   <td>
-//                   {m.vehicleId?.vehicleImage ? (
-//                     <img
-//                       src={`http://localhost:5000/uploads/${m.vehicleId?.vehicleImage}`}
-//                       alt="Vehicle"
-//                       width={100}
-//                     />
-//                   ) : (
-//                     <span>No Image</span>
-//                   )}
-//                 </td>
-
-//                 <td>{m.maintenanceType}</td>
-//                 <td>
-//                   {new Date(m.maintenanceDate).toLocaleDateString("en-GB")}
-//                 </td>
-//                 <td>{m.issuetype}</td>
-//                 <td>{m.companyName}</td>
-//                 <td>{m.MechanicName}</td>
-//                 <td>{m.status}</td>
-//                 </tr>
-//               ))}
-//             </tbody>
-//           </table>
-//         )}
-//       </div>
-//     </>
-//   );
-// };
-// export default MainteanceHistory;
-
-
 import React, { useEffect, useState } from "react";
 import styles from "./MainteanceHistory.module.css";
 import { useDispatch, useSelector } from "react-redux";
@@ -170,7 +88,7 @@ const MainteanceHistory = () => {
                   <td>
                     {m.vehicleId?.vehicleImage ? (
                       <img
-                        src={`http://localhost:5000/uploads/${m.vehicleId.vehicleImage}`}
+                        src={`https://transport-management-system-6jtl.onrender.com/uploads/${m.vehicleId.vehicleImage}`}
                         alt="Vehicle"
                         width={80}
                       />
